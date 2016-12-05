e0281245d1d3b69b9c3b25bab6c38604bd5a4141
George Galt <george@galtfamily.net>
2016-12-05 17:43:29 -0500

    stuff

--- qclickablelabel.cpp
+++ qclickablelabel.cpp
@@ -1,6 +1,27 @@
 #include "qclickablelabel.h"
 
-qClickableLabel::qClickableLabel()
+qClickableLabel::qClickableLabel(QWidget *parent, Qt::WindowFlags f) : QLabel(parent, f)
 {
 
 }
+
+void qClickableLabel::mousePressEvent(QMouseEvent *e)
+{
+
+}
+
+void qClickableLabel::mouseReleaseEvent(QMouseEvent *e)
+{
+
+}
+
+void qClickableLabel::mouseMoveEvent(QMouseEvent *e)
+{
+
+}
+
+bool qClickableLabel::hitLabel(const QPoint &pos) const
+{
+    return rect().contains(pos);
+}
+
--- qclickablelabel.h
+++ qclickablelabel.h
@@ -3,6 +3,10 @@
 
 #include <QObject>
 #include <QLabel>
+#include <QRect>
+#include <QPoint>
+
+#include <QMouseEvent>
 
 
 class qClickableLabel : public QLabel
@@ -10,11 +14,27 @@ class qClickableLabel : public QLabel
     Q_OBJECT
 
 public:
-    qClickableLabel();
+    qClickableLabel(QWidget * parent = 0, Qt::WindowFlags f = 0);
+
+protected:
+    void mousePressEvent(QMouseEvent *e);
+    void mouseReleaseEvent(QMouseEvent *e);
+    void mouseMoveEvent(QMouseEvent *e);
+
+private:
+    bool hitLabel(const QPoint &pos) const;
+
+private:
+    QRect myRect;
+    bool m_pressed;
+
 };
 
 #endif // QCLICKABLELABEL_H
 
+
+//QLabel(QWidget * parent = 0, Qt::WindowFlags f = 0)
+//QLabel(const QString & text, QWidget * parent = 0, Qt::WindowFlags f = 0)
 
 //void QAbstractButton::mousePressEvent(QMouseEvent *e)
 //{
@@ -86,3 +106,14 @@ public:
 //    }
 //}
 
+///*!
+//Returns \c true if \a pos is inside the clickable button rectangle;
+//otherwise returns \c false.
+//By default, the clickable area is the entire widget. Subclasses
+//may reimplement this function to provide support for clickable
+//areas of different shapes and sizes.
+//*/
+//bool QAbstractButton::hitButton(const QPoint &pos) const
+//{
+//    return rect().contains(pos);
+//}
